83b453baddd42cb9bfdc16f511bc9f5d21f952ac
sandrabosk <sandraboskovic@hotmail.com>
2019-05-29 23:16:52 -0400

    props drilling + refactored with context

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// REFACTORED WITH CONTEXT API:
+
+import { MyContext } from './context';
+
+function Bootcamp (){
+  return(
+    <MyContext.Consumer>
+        { context => {
+          const { hoursToGo } = context.currentUser;
+          return (
+            <div>
+              <p>Hours to invest: { hoursToGo }</p>
+              <Student  />
+            </div>
+          )
+      } }
+  </MyContext.Consumer>
+  )
+
 }
+
+
+function Student (){
+  return (
+    <MyContext.Consumer>
+    { context => {
+      console.log('the C is: ', context)
+      const { firstName } = context.currentUser; 
+      return(
+        <div>
+          <h1>Hello {firstName}!</h1>
+          <button onClick={ context.addHours }> Add Those Hours! </button>
+        </div>
+      )
+    }}
+    </MyContext.Consumer>
+  )
+}
+function App () {
+      return (
+          <Bootcamp />
+      )
+}
+
+
+
+// PROPS DRILLING:
+
+
+// function Bootcamp (props){
+//   return (
+//     <Student user = { props.user } addHours={ props.addHours }  />
+//   )
+// }
+
+// function Student (props){
+//   const { firstName } = props.user.currentUser;
+//   return (
+//     <div>
+//         <h1>Hello {firstName}!</h1>
+//         <button onClick={ props.addHours }> Add Those Hours! </button>
+//     </div>
+//   )
+// }
+// class App extends React.Component {
+//     state = {
+//       currentUser: {
+//         firstName: "Sandra",
+//         course: "Web Dev",
+//         hoursToGo: 360
+//       }
+//     }
+
+//     addHoursPlease = () => {
+//       this.setState( prev => {
+//         return {
+//           currentUser: {
+//             ...prev.currentUser,
+//             hoursToGo: prev.currentUser.hoursToGo + 10
+//           }
+//         }
+//       })
+//     }
+
+//     render (){
+//       const { hoursToGo } = this.state.currentUser;
+//       return (
+//         <div>
+//           <p>I'm in App.js ✅ <b> Hours I want to invest: { hoursToGo } </b></p>
+//           <Bootcamp  user = { this.state }  addHours={ this.addHoursPlease }/>
+//         </div>
+//       )
+//     }
+// }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { MyProvider } from './context';
+
+ReactDOM.render(
+    <MyProvider>
+        <App />
+    </MyProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
